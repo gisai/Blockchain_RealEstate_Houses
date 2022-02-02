0a84c12f8088a331f5f7fa0d3ae164eb0318b6a0
Laura Renieblas <97789606+lau2496@users.noreply.github.com>
2022-02-02 21:12:09 +0100

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -7,18 +7,19 @@ module.exports = {
 			port: 7545,
 			network_id: '*' // Match any network id
 		},
+		ropsten: {
+			provider: function() {
+			  return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161");
+			}
+		},
 		advanced: {
     
     		 gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
     
     	}
-	},
-
-	mocha: {
-
-	},
+	},	
 	
-	  // Configure your compilers
+	// Configure your compilers
 	compilers: {
 		solc: {
 		  version: "0.8.6",    
